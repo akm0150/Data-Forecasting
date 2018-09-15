b03e45a6965237994e8d07183753105bfe9fa680
akm0150 <akm.anujmehta0011@gmail.com>
2018-09-15 22:03:39 +0530

    Update plot between predicted and actual test.R

--- plot between predicted and actual test.R
+++ plot between predicted and actual test.R
@@ -10,7 +10,7 @@ df1$predicted = ts(df1$predicted,start = c(1995,1),frequency = 12)
 df1$actual = ts(df1$actual,start = c(1995,1),frequency = 12)
 
 
-plot(df1$predicted,type="l",xlab="values",ylab="stock_price",main="Comparison between predicted and actual stock price",col="blue",xlim=c(1995,2019),axes=F)
+plot(df1$predicted,type="l",xlab="values",ylab="stock_price",main=c("Comparison between predicted and actual stock price of ",x1),col="blue",xlim=c(1995,2019),axes=F)
 legend(2006,80, legend=c("actual", "predicted"),col=c("red", "blue"), lty=1:2, cex=0.8)
 
 axis(1,at=1995:2020,labels=1995:2020);axis(2);box()
