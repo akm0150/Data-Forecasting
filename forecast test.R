c4c24c368b47c7d5766593a7dc1bf08c48b27567
akm0150 <akm.anujmehta0011@gmail.com>
2019-03-31 13:25:54 +0530

    Delete forecast test.R

--- forecast test.R
+++
@@ -1,7 +0,0 @@
-fit_seasonal<-auto.arima(close_clean_data) #using auto arima function arima model is created
-tsdisplay(residuals(fit_seasonal), lag.max=45, main='Model Residuals' ) #plotting residuals
-seas_fcast2 <- forecast(fit_seasonal, h=16) # forecasting seasonalized data
-#plotting forecasted data
-plot(seas_fcast2, xlab='Years', ylab = 'Msft_Close', type="l", lwd=2, col="red",xlim=c(1995,2029),ylim=c(0,200),axes=F)
-axis(1,at=1995:2020,labels=1995:2020);axis(2);box()
-
